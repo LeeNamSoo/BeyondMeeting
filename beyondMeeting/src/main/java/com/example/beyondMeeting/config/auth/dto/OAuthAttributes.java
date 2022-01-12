35f7f1bd9b01837b6f14a711b57a7f3dea5c7c6d
유진주 <jinju6288@naver.com>
2022-01-12 15:20:27 +0900

    에러 해결.. but, mustache(프론트) 동작안됨

--- beyondMeeting/src/main/java/com/example/beyondMeeting/config/auth/dto/OAuthAttributes.java
+++ beyondMeeting/src/main/java/com/example/beyondMeeting/config/auth/dto/OAuthAttributes.java
@@ -28,13 +28,13 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
-       /* if("google".equals(registrationId)) {
+        if("google".equals(registrationId)) {
             return ofGoogle("id",attributes);
         }
-        */
+
         return ofNaver(userNameAttributeName, attributes);
     }
-    /*
+
     public static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
@@ -44,7 +44,7 @@ public class OAuthAttributes {
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
-*/
+
     public static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
         Map<String, Object> response = (Map<String,Object>) attributes.get("response");
 

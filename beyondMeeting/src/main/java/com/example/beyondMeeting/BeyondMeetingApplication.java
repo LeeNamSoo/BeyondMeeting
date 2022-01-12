d9b014909790305e58825bae25569aa10bead8e1
유진주 <jinju6288@naver.com>
2022-01-12 13:14:23 +0900

    소셜 로그인 OAuth2 - Error : Method springSecurityFilterChain in org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration required a bean of type 'org.springframework.security.oauth2.client.registration.ClientRegistrationRepository' that could not be found.

---
+++ beyondMeeting/src/main/java/com/example/beyondMeeting/BeyondMeetingApplication.java
@@ -0,0 +1,13 @@
+package com.example.beyondMeeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondMeetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyondMeetingApplication.class, args);
+	}
+
+}
---
+++ beyondMeeting/src/test/java/com/example/beyondMeeting/BeyondMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beyondMeeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeyondMeetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
